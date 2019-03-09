511973d562ba6904961efb386bc9753af84a42ee
Adam Walker <adamw207@yahoo.com>
2019-03-09 15:41:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,22 @@
-def addition(num1, num2)
+def addition(num1)+(num2)
 end
 
-def subtraction(num1, num2)
+def subtraction(num1)-(num2)
 end
 
-def division(num1, num2)
+def division(num1)/(num2)
 end
 
-def multiplication(num1, num2)
+def multiplication(num1)*(num2)
 end
 
-def modulo(num1, num2)
+def modulo(num1)%(num2)
 end
 
-def square_root(num)
+def square_root Math.sqrt(num)
 end
 
-def order_of_operation(num1, num2, num3, num4)
+def order_of_operation(num1)+((num2*num3)/num4)
+  
   #Hint:  __ + (( __ * __ ) / __ )
 end
